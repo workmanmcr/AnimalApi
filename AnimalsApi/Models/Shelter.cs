353dc615abfa4e09f2a3798c6f1bf9f07000cb53
workmanmcr <workmanmcr@gmail.com>
2023-08-24 13:42:53 -0700

    add shelter class

---
+++ AnimalsApi/Models/Shelter.cs
@@ -0,0 +1,13 @@
+namespace AnimalsApi.Models
+{
+
+  public class Shelter
+  {
+    public int ShelterId { get; set; }
+    public string Title { get; set; }
+    public string Location { get; set; }
+    public string About { get; set; }
+     
+
+  }
+}
